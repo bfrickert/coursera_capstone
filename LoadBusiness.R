c57a5a9e2fb7bfb68fa1b9d6cc1da632259133f0
Brian Frickert <bfrickert@yahoo.com>
2015-10-07 08:33:58 -0400

    LoadReview added

--- LoadBusiness.R
+++ LoadBusiness.R
@@ -1,6 +1,4 @@
 library(rjson)
-library(plyr)
-library(reshape2)
 
 json_file <- "yelp_academic_dataset_business.json"
 con = file(json_file, "r")
@@ -27,17 +25,25 @@ easy <- lapply(yelp.business, function(x){ return(list(x$business_id, x$name,
                                                        x$open, 
                                                        x$stars, x$latitude,
                                                        x$longitude,
-                                                       x$type, paste(x$categories,collapse=" | ")
+                                                       x$type, paste(x$categories,collapse=" | "),
+                                                       if(is.null(x$attributes$`By Appointment Only`)) NA else x$attributes$`By Appointment Only`,
+                                                       if(is.null(x$attributes$`Price Range`)) NA else x$attributes$`Price Range`,
+                                                       if(is.null(x$attributes$`Happy Hour`)) NA else x$attributes$`Happy Hour`,
+                                                       if(is.null(x$attributes$Smoking)) NA else x$attributes$Smoking,
+                                                       if(is.null(x$attributes$BYOB)) NA else x$attributes$BYOB,
+                                                       if(is.null(x$attributes$Corkage)) NA else x$attributes$Corkage,
+                                                       if(is.null(x$attributes$`BYOB/Corkage`)) NA else x$attributes$`BYOB/Corkage`,
+                                                       if(is.null(x$attributes$Alcohol)) NA else x$attributes$Alcohol,
+                                                       if(is.null(x$attributes$`Waiter Service`)) NA else x$attributes$`Waiter Service`
                                                        ))})
 
 
-df <-data.frame(matrix(unlist(easy), ncol=26,byrow=T))
+df <-data.frame(matrix(unlist(easy), ncol=35,byrow=T))
 names(df) <- c('business_id','name','open_sun','closed_sun','open_mon','closed_mon'
                ,'open_tues','closed_tues','open_wed','closed_wed','open_thu','closed_thu'
                ,'open_fri','closed_fri','open_sat','closed_sat', 'full_address','city','state',
-               'review_count','open','stars','lat','lon','type','categories')
+               'review_count','open','stars','lat','lon','type','categories', 'by_appt','price_range','happy_hour','smoking','byob','corkage','byob_corkage', 'alcohol', 'waiter_service')
 
 attrs <- lapply(yelp.business, function(x){ return(x$attributes)})
 attrs_names <- unique(unlist(lapply(attrs, names)))
-
 
