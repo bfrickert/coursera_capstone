b964a896b8bf6ce6e9a6922d9205592568e5a1d4
Brian Frickert <bfrickert@yahoo.com>
2015-10-08 20:12:08 -0400

    Late Night offerings

--- redshift_sql/wednesday_night.sql
+++ redshift_sql/wednesday_night.sql
@@ -7,7 +7,8 @@ join (select b.business_id, avg(r.stars*1.00000) avg_stars, count(distinct u.use
         join brian.business b on b.business_id = r.business_id 
         join (select count(8) tip_count, user_id from brian.tips t group by t.user_id) t on t.user_id = r.user_id
         group by b.business_id) r on r.business_id = b.business_id
-where user_count > 100
+where --user_count > 100 and 
+      "0-friday" + "1-friday" + "2-friday" + "3-friday" + "4-friday" + "5-friday" + "0-monday" + "1-monday" + "2-monday" + "3-monday" + "4-monday" + "5-monday" + "0-saturday" + "1-saturday" + "2-saturday" + "3-saturday" + "4-saturday" + "5-saturday" + "0-sunday" + "1-sunday" + "2-sunday" + "3-sunday" + "4-sunday" + "5-sunday" + "0-thursday" + "1-thursday" + "2-thursday" + "3-thursday" + "4-thursday" + "5-thursday" + "0-tuesday" + "1-tuesday" + "2-tuesday" + "3-tuesday" + "4-tuesday" + "5-tuesday" + "0-wednesday" + "1-wednesday" + "2-wednesday" + "3-wednesday" + "4-wednesday" + "5-wednesday" > 99
 order by relative_performance desc
 limit 22200;
 
@@ -17,3 +18,12 @@ select b.categories, r.* from brian.reviews r join brian.business b on b.busines
 where (lower(r.review) like '%lobster%' and lower(b.categories) not like '%restaurant%') 
    --or b.categories like '%seafood%'
    ; 
+
+
+select ch.business_id from brian.checkins ch
+where "0-friday" + "1-friday" + "2-friday" + "3-friday" + "4-friday" + "5-friday" + "0-monday" + "1-monday" + "2-monday" + "3-monday" + "4-monday" + "5-monday" + "0-saturday" + "1-saturday" + "2-saturday" + "3-saturday" + "4-saturday" + "5-saturday" + "0-sunday" + "1-sunday" + "2-sunday" + "3-sunday" + "4-sunday" + "5-sunday" + "0-thursday" + "1-thursday" + "2-thursday" + "3-thursday" + "4-thursday" + "5-thursday" + "0-tuesday" + "1-tuesday" + "2-tuesday" + "3-tuesday" + "4-tuesday" + "5-tuesday" + "0-wednesday" + "1-wednesday" + "2-wednesday" + "3-wednesday" + "4-wednesday" + "5-wednesday" > 99 limit 443;
+
+select column_name
+from information_schema.columns where table_schema = 'brian' and 
+table_name like '%checkins%' and substring(column_name, 1, 2) in ('0-','1-', '2-', '3-', '4-','5-') order by substring(column_name, 3,4), substring(column_name, 1, 2);
+
